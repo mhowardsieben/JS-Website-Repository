e37be9025fb318e8e1092a4ba7224f54dfaa3274
mhowardsieben <71395726+mhowardsieben@users.noreply.github.com>
2022-02-23 13:59:13 -0700

    cccc

--- js/vote.js
+++ js/vote.js
@@ -1,15 +1,9 @@
-var counterplus = document.getElementsByClassName('upvoteButton');
-var counterminus = document.getElementsByClassName('downvoteButton');
-var counterdisplay = document.getElementsByClassName('counter');
+var counterplus = document.getElementById('upvoteButton');
+var counterminus = document.getElementById('downvoteButton');
+var counterdisplay = document.getElementById('counter');
 var count = 0;
-
-updateDisplay();
 
 counterplus.onclclick = function(){
   count += 1;
   updateDisplay();
-}
-
-function updateDisplay() {
-  counterdisplay.innerhtml = count;
 }
