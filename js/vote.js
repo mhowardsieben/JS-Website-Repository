81d299aed63dd4aec9cecd36774c058602e3e9c3
mhowardsieben <71395726+mhowardsieben@users.noreply.github.com>
2022-02-24 14:59:17 -0700

    Update vote.js

--- js/vote.js
+++ js/vote.js
@@ -2,23 +2,31 @@ const counterplus = document.querySelector('.upvoteButton');
 const counterminus = document.querySelector('.downvoteButton');
 let count = document.querySelector('.counter');
 
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
 let upVote = false;
-let downVote = false;
 
 counterplus.addEventListener('click', function() {
-  let downVote = false
-  if (downVote === false) {
+  let random = getRandomInt(10);
+  console.log(random);
+  if (random == 0) {
+    upVote = true
+    console.log("upvote = " + upVote);
+  } else if (random > 0) {
+    upVote = false;
+    console.log('upvote = ' + upVote)
+  }
+  if (upVote === true) {
+    count.innerHTML = parseInt(count.innerHTML) + 1;
+    console.log('if 0')
+  } else if (upVote === false) {
     count.innerHTML = parseInt(count.innerHTML) - 1;
+    console.log('if not 0')
   }
 });
 
 counterminus.addEventListener('click', function() {
-  let upVote = true
-  if (upVote === true) {
     count.innerHTML = parseInt(count.innerHTML) - 1;
-  }
 });
