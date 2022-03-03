3afeb64f3488c81285df0cd24142ca5297f5b495
mhowardsieben <71395726+mhowardsieben@users.noreply.github.com>
2022-03-02 22:48:09 -0700

    cummit

--- js/globalVariables.js
+++ js/globalVariables.js
@@ -18,7 +18,16 @@ var post = document.getElementById('post');
 var loginButton = document.getElementById('Login');
 var signoutButton = document.getElementById('signOut');
 var username = localStorage.getItem("username");
+var loggedIn = false;
 var loggedIn = localStorage.getItem("loggedIn");
 let count = document.querySelector('.counter');
 let upVote = false;
 console.log(loggedIn)
+if (loggedIn === true) {
+  window.location.href = "childPages\\main.html";
+  console.log("Its Working")
+}
+else if (loggedIn === false) {
+  console.log("Signed Out")
+  window.location.replace("..\\Index.html");
+}
